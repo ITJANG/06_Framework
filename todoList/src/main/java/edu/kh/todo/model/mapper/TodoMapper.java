0c5c9d5ee74657d16a0902ff73494af8b010a917
ITJANG <whalswkd1213@gmail.com>
2025-04-18 17:41:32 +0900

    요상한 fetch

--- todoList/src/main/java/edu/kh/todo/model/mapper/TodoMapper.java
+++ todoList/src/main/java/edu/kh/todo/model/mapper/TodoMapper.java
@@ -39,6 +39,14 @@ public interface TodoMapper {
 
 	Todo todoDetail(int todoNo);
 
+	int changeComplete(Todo todo);
+
+	int todoUpdate(Todo todo);
+
+	int todoDelete(Todo todo);
+
+	int getTotalCount();
+
 	
 	
 	
